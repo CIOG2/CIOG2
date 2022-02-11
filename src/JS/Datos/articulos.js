9b8fd0774d8e274971fa7785751732a5f7613a72
Carlos Ivan <carlos.ivan10043@gmail.com>
2022-02-11 12:01:11 -0700

    Correcion de unos typos

--- src/JS/Datos/articulos.js
+++ src/JS/Datos/articulos.js
@@ -1,7 +1,7 @@
 const articulos = [
     {
         title: '¿Sabes que tan segura es tu contraseña?',
-        subtitle: 'Los elefangruesa, en el orden, ahora inválido, de los paquidermos. Existen hoy en día tres especies y diversas subespecies.',
+        subtitle: 'Una computadora promedio puede realizar 4 mil millones de cálculos por segundo, con esa potencia de cálculo se podrían probar los 100 millones de combinaciones al instante ',
         image: 'https://i.ibb.co/mbdDfbS/Seguridad-Mini.jpg',
         ImageAlt: 'Imagen de un candado digital',
         link: 'https://ciog2.github.io/CIOG2/Seguridad/que-tan-segura-es-tu-contrasena.html',
