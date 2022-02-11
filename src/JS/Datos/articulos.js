ab5f23e057c24ee36b79ff391f94af4e63f4a751
Carlos Ivan <carlos.ivan10043@gmail.com>
2022-02-11 11:49:54 -0700

    Primer post del blog terminado¿

--- src/JS/Datos/articulos.js
+++ src/JS/Datos/articulos.js
@@ -4,7 +4,7 @@ const articulos = [
         subtitle: 'Los elefangruesa, en el orden, ahora inválido, de los paquidermos. Existen hoy en día tres especies y diversas subespecies.',
         image: 'https://i.ibb.co/mbdDfbS/Seguridad-Mini.jpg',
         ImageAlt: 'Imagen de un candado digital',
-        link: 'https://www.youtube.com/'
+        link: 'https://ciog2.github.io/CIOG2/Seguridad/que-tan-segura-es-tu-contrasena.html',
     },
 ]
 
