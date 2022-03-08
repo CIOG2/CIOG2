32c169c4f368bb9c77553b74fc27285c6bc63f82
Carlos Ivan <carlos.ivan10043@gmail.com>
2022-03-08 13:26:45 -0700

    links del header dinamicos

---
+++ src/JS/componentes/links.js
@@ -0,0 +1,25 @@
+//Links en las pantallas grandes
+const blog = document.getElementById('blog-link');
+const portafolio = document.getElementById('portafolio-link');
+const sobreMi = document.getElementById('sobreMi-link');
+const contato = document.getElementById('contato-link');
+
+//Links en la seccion movil
+const blogMobile = document.getElementById('blog-mobile-link');
+const portafolioMobile = document.getElementById('portafolio-mobile-link');
+const sobreMiMobile = document.getElementById('sobreMi-mobile-link');
+const contatoMobile = document.getElementById('contato-mobile-link');
+
+
+
+blog.href = 'https://ciog2.github.io/CIOG2/blogMain.html';
+blogMobile.href = 'https://ciog2.github.io/CIOG2/blogMain.html';
+
+portafolio.href = 'https://ciog2.github.io/CIOG2/portafolio.html';
+portafolioMobile.href = 'https://ciog2.github.io/CIOG2/portafolio.html';
+
+sobreMi.href = 'https://ciog2.github.io/CIOG2/SobreMi.html';
+sobreMiMobile.href = 'https://ciog2.github.io/CIOG2/SobreMi.html';
+
+contato.href = 'https://ciog2.github.io/CIOG2/contacto.html';
+contatoMobile.href = 'https://ciog2.github.io/CIOG2/contacto.html';
