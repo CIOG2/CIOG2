4bee44d3982537a5ef4fa7cee837d5aef6868e64
Carlos Ivan <carlos.ivan10043@gmail.com>
2022-03-08 13:53:48 -0700

    Links del footer dinamicos

--- src/JS/componentes/links.js
+++ src/JS/componentes/links.js
@@ -1,25 +1,56 @@
+//HEADER
 //Links en las pantallas grandes
 const blog = document.getElementById('blog-link');
 const portafolio = document.getElementById('portafolio-link');
 const sobreMi = document.getElementById('sobreMi-link');
 const contato = document.getElementById('contato-link');
-
 //Links en la seccion movil
 const blogMobile = document.getElementById('blog-mobile-link');
 const portafolioMobile = document.getElementById('portafolio-mobile-link');
 const sobreMiMobile = document.getElementById('sobreMi-mobile-link');
 const contatoMobile = document.getElementById('contato-mobile-link');
 
-
-
 blog.href = 'https://ciog2.github.io/CIOG2/blogMain.html';
-blogMobile.href = 'https://ciog2.github.io/CIOG2/blogMain.html';
-
 portafolio.href = 'https://ciog2.github.io/CIOG2/portafolio.html';
-portafolioMobile.href = 'https://ciog2.github.io/CIOG2/portafolio.html';
-
 sobreMi.href = 'https://ciog2.github.io/CIOG2/SobreMi.html';
-sobreMiMobile.href = 'https://ciog2.github.io/CIOG2/SobreMi.html';
-
 contato.href = 'https://ciog2.github.io/CIOG2/contacto.html';
+
+blogMobile.href = 'https://ciog2.github.io/CIOG2/blogMain.html';
+portafolioMobile.href = 'https://ciog2.github.io/CIOG2/portafolio.html';
+sobreMiMobile.href = 'https://ciog2.github.io/CIOG2/SobreMi.html';
 contatoMobile.href = 'https://ciog2.github.io/CIOG2/contacto.html';
+
+
+
+//FOOTER
+//Variables para los links del footer
+const tiktok = document.getElementById('tiktok-link');
+const discord = document.getElementById('discord-link');
+const instagram = document.getElementById('instagram-link');
+const linkedin = document.getElementById('linkedin-link');
+const github = document.getElementById('github-link');
+const twitter = document.getElementById('twitter-link');
+//Variables las imagenes
+const tiktokImage = document.getElementById('tiktok-image');
+const discordImage = document.getElementById('discord-image');
+const instagramImage = document.getElementById('instagram-image');
+const linkedinImage = document.getElementById('linkedin-image');
+const githubImage = document.getElementById('github-image');
+const twitterImage = document.getElementById('twitter-image');
+
+
+//Links del footer
+tiktok.href = 'https://www.tiktok.com/@ciog_2?lang=es';
+discord.href = 'https://discord.gg/fDtjGbAkR5'
+instagram.href = 'https://www.instagram.com/ciog_2/';
+linkedin.href = 'https://www.linkedin.com/in/carlos-ivan-ochoa-gomez-90bb1b20a/';
+github.href = 'https://github.com/CIOG2';
+twitter.href = 'https://twitter.com/GoCarlos';
+
+//links de las imagenes de redes sociales
+tiktokImage.src = 'https://i.ibb.co/FKcgvKX/tik-tok.png';
+discordImage.src = 'https://i.ibb.co/9rML6wG/discord.png';
+instagramImage.src = 'https://i.ibb.co/W6Ty3yj/instagram.png';
+linkedinImage.src = 'https://i.ibb.co/nLxLLtd/linkedin.png';
+githubImage.src = 'https://i.ibb.co/1RQsbkg/github.png';
+twitterImage.src = 'https://i.ibb.co/cT8S9JJ/twiter.png';
